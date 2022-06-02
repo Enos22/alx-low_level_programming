465194c281b748781ecda39a4a9974a90a922ec5
Enos22 <enos.arenga@gmail.com>
2022-06-02 15:58:28 -0700

    C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
-/**
- * main - entry point
- *
- * return:if  success 0
- * *
- * task program.
- */
-int main(void)
-{
-int i;
-char c;
-long int l;
-float f;
-long long int ll;
 
-printf("Size of a char: %lu byte(s)\n", sizeof(c));
-printf("Size of an int: %lu bytes(s)\n", sizeof(i));
-printf("Size of a long int: %lu bytes(s)\n", sizeof(l));
-printf("Size of a long long int: %lu bytes(s)\n", sizeof(ll));
-printf("Size of a float: %lu bytes(s)\n", sizeof(f));
-return (0);
+/**
+ *
+ * * main - print the various types in their different size of data
+ *
+ * *
+ *
+ * * Return: 0
+ *
+ * */
+
+int main(void)
+
+{	
+
+	char c;	
+
+	int n;	
+
+	long g;	
+
+	long long lg;	
+
+	float f;
+
+		
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));	
+
+	printf("Size of an int: %lu byte(s)\n", sizeof(n));	
+
+	printf("Size of a long int: %lu byte(s)\n", sizeof(g));	
+
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lg));	
+
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+
+		return (0);
+
 }
