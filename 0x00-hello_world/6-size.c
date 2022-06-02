5910a2666213f42abd349ba06c1bfb44f66160b5
Enos22 <enos.arenga@gmail.com>
2022-06-02 13:47:54 -0700

    C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,19 +6,20 @@
  * task 6 program
  * */
 int main(void)
+{
+	int i;
+	char c;
+	long int l;
+	float f;
+	long long int ll;
 
-	int i = 4;
-	char c = 1;
-	long int l = 8;
-	float f = 4;
-	long long int ll = 8;
+	/* evaluating the size of varaibles*/
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(l));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(ll));
-	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n",sizeof(c));
+	printf("Size of an int: %lu bytes(s)\n",sizeof(i));
+	printf("Size of a long int: %lu bytes(s)\n",sizeof(l));
+	printf("Size of a long long int: %lu bytes(s)\n",sizeof(ll));
+	printf("Size of a float: %lu bytes(s)\n",sizeof(f));
 
-
-	return (0);
+	return 0;
 }
