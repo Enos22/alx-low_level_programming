6719573b323a4bed5738ed4255a91b0e979a2748
Enos22 <enos.arenga@gmail.com>
2022-06-03 14:17:24 -0700

    a program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints all possible combinations of single-digit numbers
+ *
+ *  betty style doc for function main goes there
+ *
+ *  Return: 0 if success
+ */
+int main(void)
+{
+	printf("0, 1, 2, 3, 4, 5, 6, 7, 8, 9\n");
+	return (0);
+}
