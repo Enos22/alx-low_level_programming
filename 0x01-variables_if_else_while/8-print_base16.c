1ba68b4db082f809f790a11addb41194954c7edf
Enos22 <enos.arenga@gmail.com>
2022-06-03 14:06:46 -0700

    a program that prints all the numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,14 +3,14 @@
 #include <time.h>
 /**
  * main - prints all the numbers of base 16 in lowercase
- * 
+ *
  * betty style doc for function main goes there
  * Return: 0 if success
  */
 int main(void)
 {
 	int i = '0';
-		
+
 	while (i <= '9')
 	{
 	putchar(i);
