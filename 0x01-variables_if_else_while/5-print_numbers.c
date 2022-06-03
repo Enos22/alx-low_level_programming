e9762301ef0ea93ff8c1489d0fe580ce06840bda
Enos22 <enos.arenga@gmail.com>
2022-06-03 09:29:45 -0700

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - -prints all single digit numbers of base 10 starting from 0
+ * betty style doc for function main goes there
+ */
+int main(void)
+{
+	srand(time(0));
+	printf("0123456789\n");
+	return (0);
+}
