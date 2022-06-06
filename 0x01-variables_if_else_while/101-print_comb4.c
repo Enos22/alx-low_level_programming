48b20ea1f0d90c9dfa9c82ff231fbb8c0dcb772f
Enos22 <enos.arenga@gmail.com>
2022-06-06 07:58:00 -0700

    a program that prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - program that prints all possible different combinations of three digits'
+ * main - prints all possible different combinations of three digits
  * Return: 0 on success
  *
  */
@@ -13,14 +13,14 @@ for (i = 48; i < 58; i++)
 {
 	for (j = 48; j < 58; j++)
 	{
-		for(k = 48; k <58; k++)
+		for (k = 48; k < 58; k++)
 		{
-			if (i !=j && i != k && j < k && i < j)
+			if (i != j && i != k && j < k && i < j)
 			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if (!(i ==55 && j == 56 && k ==57))
+				if (!(i == 55 && j == 56 && k == 57))
 				{
 					putchar(44);
 					putchar(32);
