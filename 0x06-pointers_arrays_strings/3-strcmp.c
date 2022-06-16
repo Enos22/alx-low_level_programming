9ae94f9a1ffa14d0dabf7c967b9df72809a1ff5a
Enos22 <enos.arenga@gmail.com>
2022-06-16 05:48:03 -0700

    function that compares two strings using strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,10 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	for (; *s1 == *s2;)
-	++s1;
-	++s2;
-		if (*s1 == 0)
-			return (0);
-	return (*(unsigned char *)s1 < *(unsigned char *)s2 ? -1 : 1);
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			break;
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
