fbcb012dc1af1f1057c87fd0fc3b8481d99fff77
Enos22 <enos.arenga@gmail.com>
2022-06-16 19:29:40 -0700

    function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * main - check the code for
+ * @p: value of leets
  * Return: Always 0.
  */
 int main(void)
 {
 	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
 	char *p;
-
 	p = leet(s);
+
 	printf("%s", p);
 	printf("%s", s);
 	return (0);
