2027869dae446bebe57a96f4a3244330cf0ce341
Enos22 <enos.arenga@gmail.com>
2022-06-22 13:46:43 -0700

    compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,6 +3,7 @@
  * wildcmp - compares two strings
  * @s1: pointer to string parameter
  * @s2: pointer to string parameter
+ * Return: 0
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -11,7 +12,7 @@ if (*s1 == '\0')
 if (*s2 != '\0' && *s2 == '*')
 {
 return (wildcmp(s1, s2 + 1));
-{
+}
 return (*s2 == '\0');
 }
 if (*s2 == '*')
@@ -23,5 +24,4 @@ else if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
 }
 return (0);
-}
 }
