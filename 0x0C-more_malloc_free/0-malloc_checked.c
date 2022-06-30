9c7d5ecb2d27251c0fdd193ef35a2b07a727bb40
Enos22 <enos.arenga@gmail.com>
2022-06-30 11:24:27 -0700

    function that allocates memory using malloc.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-/** malloc_checked - function that allocates memory
+/**
+ * malloc_checked - function that allocates memory
  * @b: the size to allocate
  *
  * Return: Nothing
