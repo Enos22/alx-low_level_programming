35b4ff053206aad694e51b7ec9306dc4d07672a0
Enos22 <enos.arenga@gmail.com>
2022-07-06 11:34:32 -0700

    prototypes

--- alx-low_level_programming/function_pointers.h
+++ alx-low_level_programming/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef function_pointer_h
 #define function_pointer_h
-#include <stlib.h>
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
