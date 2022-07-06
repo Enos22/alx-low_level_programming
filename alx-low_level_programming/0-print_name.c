7e1bd68915d91f75a4850e339f4933546b3ceea0
Enos22 <enos.arenga@gmail.com>
2022-07-06 11:30:35 -0700

    function that prints a name

--- alx-low_level_programming/0-print_name.c
+++ alx-low_level_programming/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 /**
  * print_name - prints name
- * @name: name to be printed 
- * @p: pointer to a function that prints a name
+ * @name: name to be printed
+ * @f: pointer to a function that prints a name
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || p == NULL)
-			return;
-	p(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
