076331f6614c8fd8abe00ca920f602540b8d2cca
Enos22 <Enos22>
2022-07-19 00:33:56 -0700

    function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,11 @@
 #include "lists.h"
 /**
  * print_listint - prints all elements of a listint_t list.
+ *
  * @h: head of linked list
+ * 
  * Return: number of nodes
+ *
  */
 
 size_t print_listint(const listint_t *h)
